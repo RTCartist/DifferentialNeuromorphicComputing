6695902ba214d611b78ba84b1173e7a9f6591256
Shengbo_Wang <100296216+RTCartist@users.noreply.github.com>
2023-12-04 22:12:02 +0800

    Update LTspice_simulation.py

--- code/LTspice_simulation.py
+++ code/LTspice_simulation.py
@@ -6,7 +6,7 @@ for row_i in range(33, 46):
     for col_j in range(1, 97):
         netlist = SpiceEditor("visual627.asc")
         netlist.add_instructions(".tran 0 460 0 0.01")
-        value_settings = {'vup': 0.18, 'vun': 0.01, 'bia1': 0.14, 'plus1': 1, 'bia2': 0.16, 'plus2': 1, 'bia0': -0.10}
+        value_settings = {'vup': 0.16, 'vun': 0.01, 'bia1': 0.12, 'plus1': 1, 'bia2': 0.14, 'plus2': 1, 'bia0': -0.10}
         netlist.set_parameters(**value_settings)
         pwl_file_name = '{}_{}.txt'.format(row_i, col_j)
         netlist.set_component_value('V1', 'PWL file=' + origin_path + pwl_file_name)
